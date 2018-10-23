f480196754e93548656cae0e1aec1b47e4ed926c
Wataru Kanzaki <kanzwataru@gmail.com>
2018-10-22 22:42:32 -0400

    SDL video stuff (not yet fully working) * start in fullscreen (for now) * rework screen drawing buffers * fit screen inside of different aspect ratio

--- src/HustleEngine/platform/sdl/video.c
+++ src/HustleEngine/platform/sdl/video.c
@@ -2,6 +2,7 @@
  * SDL1.2 backend Video module
 */
 #include "platform/video.h"
+#include "common/math.h"
 #include <SDL/SDL.h>
 #include <stdio.h>
 
@@ -16,31 +17,64 @@
 
 static SDL_Surface *screen;   /* screen surface (scaled) */
 static SDL_Surface *framebuffer; /* based on render.c buffer, at original resolution*/
-static SDL_Rect     fbrect = {0, 0, 0, 0};
+static SDL_Rect     native_resolution;
+static SDL_Rect     original_resolution;
+static SDL_Rect     logical_screen;
 static int scale;
 static byte video_mode;
 
 SDL_Color fbpalette[256];
+
+static SDL_Rect get_native_resolution(void)
+{
+    SDL_Rect **modes;
+    int i;
+    
+    modes = SDL_ListModes(NULL, SDL_FULLSCREEN);
+    if(modes == NULL)
+        PANIC("No modes");
+    
+    if(modes == (SDL_Rect **)-1) {
+        PANIC("All resolutions available. What does that mean?");
+    }
+    else {
+        printf("Available Modes: \n\n");
+        for(i = 0; modes[i]; ++i)
+            printf("->  %d x %d\n", modes[i]->w, modes[i]->h);
+    }
+    
+    return *(modes[0]);
+}
+
+static SDL_Rect fit_rect(SDL_Rect large, SDL_Rect small, int *scale_out)
+{
+    float scale = MIN(large.w / small.w, large.h / small.h);
+    
+    SDL_Rect out = {0, 0, small.w * (int)scale, small.h * (int)scale};
+    
+    *scale_out = (int)scale;
+    return out;
+}
+
 /*
  * Video API init
 */
 void video_init_mode(byte mode, byte scaling)
 {
     assert(scaling > 0);
-    scale = scaling;
     video_mode = mode;
     switch(mode) {
         case VIDEO_MODE_LOW256:
-            fbrect.w = LOW256_WIDTH;
-            fbrect.h = LOW256_HEIGHT;
+            original_resolution.w = LOW256_WIDTH;
+            original_resolution.h = LOW256_HEIGHT;
             break;
         case VIDEO_MODE_HIGH16:
-            fbrect.w = HIGH16_WIDTH;
-            fbrect.h = HIGH16_HEIGHT;
+            original_resolution.w = HIGH16_WIDTH;
+            original_resolution.h = HIGH16_HEIGHT;
             break;
         case VIDEO_MODE_HIGH16HALF:
-            fbrect.w = HIGH16HALF_WIDTH;
-            fbrect.h = HIGH16HALF_HEIGHT;
+            original_resolution.w = HIGH16HALF_WIDTH;
+            original_resolution.h = HIGH16HALF_HEIGHT;
             break;
         default:
             PANIC("Invalid video mode");
@@ -52,16 +86,20 @@ void video_init_mode(byte mode, byte scaling)
         exit(1);
     }
     
-    screen = SDL_SetVideoMode(fbrect.w * scale, fbrect.h * scale, 24, SDL_SWSURFACE | SDL_ANYFORMAT);
+    native_resolution = get_native_resolution();
+    logical_screen    = fit_rect(native_resolution, original_resolution, &scale);
+    
+    /* use the native resolution and then we can handle the scaling ourselves */
+    screen = SDL_SetVideoMode(native_resolution.w, native_resolution.h, 24, SDL_SWSURFACE | SDL_ANYFORMAT | SDL_FULLSCREEN);
     if(!screen) {
-        fprintf(stderr, "Failed to set video mode (%d x %d)\n%s\n", fbrect.w * scale, fbrect.h * scale, SDL_GetError());
+        fprintf(stderr, "Failed to set video mode (%d x %d)\n%s\n", original_resolution.w * scale, original_resolution.h * scale, SDL_GetError());
         exit(1);
     }
     
-    /* framebuffer is 8-bit indexed (256 col) just like VGA mode 13h */
-    framebuffer = SDL_CreateRGBSurface(SDL_SWSURFACE, fbrect.w, fbrect.h, 8, 0,0,0,0);
+    /* framebuffer colour mode is 8-bit indexed (256 col) */
+    framebuffer = SDL_CreateRGBSurface(SDL_SWSURFACE, logical_screen.w, logical_screen.h, 8, 0,0,0,0);
     if(!framebuffer) {
-        fprintf(stderr, "Failed to create framebuffer (%d x %d)\n%s\n", fbrect.w, fbrect.h, SDL_GetError());
+        fprintf(stderr, "Failed to create framebuffer (%d x %d)\n%s\n", original_resolution.w, original_resolution.h, SDL_GetError());
         exit(1);
     }
 }
@@ -97,8 +135,9 @@ void video_flip(buffer_t *backbuf)
     
     if(SDL_MUSTLOCK(framebuffer))
         SDL_LockSurface(framebuffer);
-        
-    memcpy(framebuffer->pixels, backbuf, fbrect.w * fbrect.h);
+    
+    memset(framebuffer->pixels, 3, logical_screen.w * logical_screen.h);
+    //memcpy(framebuffer->pixels, backbuf, original_resolution.w * original_resolution.h);
     //*((buffer_t *)framebuffer->pixels) = 4;
     
     if(SDL_MUSTLOCK(framebuffer))
